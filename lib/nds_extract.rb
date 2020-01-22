7d94d4e2fa12eb78135a3b7a9cd955a17c841946
William Harris <willmharris0@gmail.com>
2020-01-22 02:47:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,3 +20,30 @@ p result
 #nil
 end
 
+test_array = [{:name=>"Stephen Spielberg",
+  :movies=>
+   [{:title=>"Jaws",
+     :studio=>"Universal",
+     :worldwide_gross=>260000000,
+     :release_year=>1975},
+    {:title=>"Close Encounters of the Third Kind",
+     :studio=>"Columbia",
+     :worldwide_gross=>135189114,
+     :release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",
+     :studio=>"Paramount",
+     :worldwide_gross=>248159971,
+     :release_year=>1981},
+    {:title=>"E.T. the Extra-terrestrial",
+     :studio=>"Universal",
+     :worldwide_gross=>435110554,
+     :release_year=>1982},
+    {:title=>"Schindler's List",
+     :studio=>"Universal",
+     :worldwide_gross=>96898818,
+     :release_year=>1993},
+    {:title=>"Lincoln",
+     :studio=>"Buena Vista",
+     :worldwide_gross=>182207973,
+     :release_year=>2012}]}]
+  p test_array 
